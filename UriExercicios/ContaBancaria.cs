c0a511403a983cbaa3bf280b6b4b78db17b65513
Daniel Céspedes <prof.dancespedes@outlook.com>
2020-04-12 12:36:26 -0300

    Proejeto teste para git

---
+++ UriExercicios/ContaBancaria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace UriExercicios {
+    class ContaBancaria {
+
+        public int Numero;
+        public string Titular;
+        public double Saldo;
+       
+
+
+    }
+}
